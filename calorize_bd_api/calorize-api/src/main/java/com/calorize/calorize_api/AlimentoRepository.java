e92a96b2aac16b06d849a8f940532753466e982d
Gustavo Rodrigues Martins <gustavorod832@gmail.com>
2025-06-06 03:51:50 -0300

    s

--- calorize_bd_api/calorize-api/src/main/java/com/calorize/calorize_api/AlimentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.calorize.calorize_api;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface AlimentoRepository extends JpaRepository<Alimento, Long> {
-    List<Alimento> findByNomeContainingIgnoreCase(String nome);
-
-
-}
-
